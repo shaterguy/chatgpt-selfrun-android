2876bc55f39dc1bca140a9ae59e1a4c130461e2f
SHatergUY <108107617+shaterguy@users.noreply.github.com>
2026-08-28 20:50:13 +0900

    test(v2): align dev5 TEST app identity assertion

--- app/src/test/java/com/shaterguy/chatgptselfrun/TestAppVariantPolicyTest.java
+++ app/src/test/java/com/shaterguy/chatgptselfrun/TestAppVariantPolicyTest.java
@@ -36,8 +36,8 @@ public final class TestAppVariantPolicyTest {
 
     @Test public void developmentVersionUsesCurrentV2Identity() throws Exception {
         String gradle = read("app/build.gradle", "build.gradle");
-        assertTrue(gradle.contains("selfRunDriveVersionName = '2.0.0-dev4'"));
-        assertTrue(gradle.contains("selfRunDriveVersionCode = 2000004"));
+        assertTrue(gradle.contains("selfRunDriveVersionName = '2.0.0-dev5'"));
+        assertTrue(gradle.contains("selfRunDriveVersionCode = 2000005"));
     }
 
     @Test public void v2DevPushUsesOnlyDedicatedV2TestWorkflow() throws Exception {
