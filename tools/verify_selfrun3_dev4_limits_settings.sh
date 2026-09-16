abeee92a44aa06766c2805c7c058fa72bbf204b1
SHatergUY <108107617+shaterguy@users.noreply.github.com>
2026-09-16 19:47:08 +0900

    ci: advance SelfRun 3 candidate preflight pin to dev7

--- tools/verify_selfrun3_dev4_limits_settings.sh
+++ tools/verify_selfrun3_dev4_limits_settings.sh
@@ -46,8 +46,8 @@ for file in "$ENGINE" "$DRIVE" "$WATCHDOG" "$PROTOCOL" "$PROBE" "$INPUT" "$IMMED
   test -s "$file"
 done
 
-grep -Fq "selfRunDriveVersionCode = 3025006" "$BUILD"
-grep -Fq "selfRunDriveVersionName = '3.2.5-dev6'" "$BUILD"
+grep -Fq "selfRunDriveVersionCode = 3025007" "$BUILD"
+grep -Fq "selfRunDriveVersionName = '3.2.5-dev7'" "$BUILD"
 
 # Former product payload ceilings must not survive in runtime code.
 ! grep -Fq 'MAX_RESULT_BYTES' "$ENGINE"
